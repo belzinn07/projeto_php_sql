24e594c2266165bc3ed5642eef2ee24bd9c62489
belzinn07 <netobelmiro1@gmail.com>
2024-05-16 14:37:06 -0300

    agor DEU CCERTO

--- pesquisa.php
+++ pesquisa.php
@@ -9,13 +9,20 @@
 </head>
 
 <body>
+    <?php
+    $pesquisa= $_POST['busca']??'';
+    include"conexao.php";
+    $sql = "SELECT * FROM usuario where nome LIKE '%$pesquisa%'";//selecione toda a tabela usuario onde a coluna nome tenha um LIKE que filtre todas as letras qu e o usuário digita
+    $dados = mysqli_query($conexao,$sql);
+    
+    ?>
     <div class="container">
         <div class="row">
             <div class="col">
                 <h1>Pesquisar</h1>
                 <nav class="navbar navbar-light bg-light">
                     <form class="form-inline" method="post" action="pesquisa.php">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Digite a sua busca" aria-label="Search">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Digite a sua busca" aria-label="Search" name="busca">
                         <br>
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
                     </form>
@@ -40,8 +47,9 @@
                             echo "<tr>
         <td>$nome</td>
         <td>$email</td>
-        <td width=150px><a href='editarcad.php? id=$id' class='btn btn-succes btn-sm>Editar</a>
-          <a href='' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#confirma'>Excluir</a>
+        <td width=150px>
+        <a href='editcad.php? id=$id' class='btn btn-success btn-sm'>Editar</a>
+          <a href='#' class='btn btn-danger btn-sm' >Excluir</a>
           </td>
         </tr>";
                         }
